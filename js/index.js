40054d0a52f10281dc31b9195cb685f1de811339
SaraMoreno98 <saramo.graphic@gmail.com>
2024-08-09 10:00:51 +0200

    Commit

--- js/index.js
+++ js/index.js
@@ -3,11 +3,33 @@ const nameInputContainer = document.getElementById('name-input-container')
 const nameInput = document.getElementById ('name-input')
 const submitButton = document.getElementById('submit-name')
 
+// GUARDAR NOMBRE EN EL LOCAL STORAGE
 function guardarNombre () {
     // LEO LO QUE HAY EN EL INPUT Y LO GUARDO EN NAME
     const name = nameInput.value.trim()
-    saludoTxt.textContent = `¡Hola, ${name}!`
+    if (name.length > 1){
+        // CREA UN ITEM CON UN VALOR DE CLAVE Y UN NOMBRE DE USER
+        localStorage.setItem('nombreUsuario', name)
+        mostrarNombre(name)
+    }
+}
+
+function mostrarNombre (name){
     nameInputContainer.style.display = 'none'
+    const hora = new Date().getHours()
+    //const hora = 20
+    parseInt(hora)
+        console.log(hora)
+
+    if(hora >= 7 && hora < 12){
+        saludoTxt.textContent = `¡Buenos días, ${name}!`
+    }
+    else if (hora >= 12 && hora < 20){
+        saludoTxt.textContent = `¡Buenas tardes, ${name}!`
+    }
+    else{            
+        saludoTxt.textContent = `¡Buenas noches, ${name}!`
+    }
 }
 
 //HACE QUE AL PULSAR GUARDAR CUMPLA LA FUNCION GUARDARNOMBRE
@@ -18,3 +40,11 @@ nameInput.addEventListener ('keypress', function (e){
         guardarNombre()
     }
 })
+
+// COMPROBAR SI YA HAY UN NOMBRE GUARDADO
+// SI HAY UN NOMBRE GUARDADO LO MUESTRO
+// LEO EL LOCAL STORAGE Y GUARDO EL NOMBRE
+const nombreGuardado = localStorage.getItem('nombreUsuario')
+if (nombreGuardado) {
+    mostrarNombre(nombreGuardado)
+}
