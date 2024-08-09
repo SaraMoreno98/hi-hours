295f5b43cfb8cc6866573c23348177f7f0557b96
SaraMoreno98 <saramo.graphic@gmail.com>
2024-08-09 09:22:41 +0200

    Commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 const saludoTxt = document.getElementById('saludo')
 const nameInputContainer = document.getElementById('name-input-container')
-const nameInput = document.getElementById ('name-inputr')
+const nameInput = document.getElementById ('name-input')
 const submitButton = document.getElementById('submit-name')
 
 function guardarNombre () {
-    console.log('Guardando el nombre')
+    // LEO LO QUE HAY EN EL INPUT Y LO GUARDO EN NAME
+    const name = nameInput.value.trim()
+    saludoTxt.textContent = `¡Hola, ${name}!`
+    nameInputContainer.style.display = 'none'
 }
 
+//HACE QUE AL PULSAR GUARDAR CUMPLA LA FUNCION GUARDARNOMBRE
 submitButton.addEventListener ('click', guardarNombre)
-
+//HACE QUE AL PULSAR EL BTN ENTER CUMPLA LA FUNCION GUARDARNOMBRE
 nameInput.addEventListener ('keypress', function (e){
     if (e.key === 'Enter'){
         guardarNombre()
